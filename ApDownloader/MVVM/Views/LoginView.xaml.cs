2a7ea1155f7134308ffe8c6df1d9467674703578
Paul Kirby <kirby.paulh@gmail.com>
2021-12-19 10:12:34 -0500

    Fixed password box

--- ApDownloader/MVVM/Views/LoginView.xaml.cs
+++ ApDownloader/MVVM/Views/LoginView.xaml.cs
@@ -8,20 +8,39 @@ namespace ApDownloader.MVVM.Views;
 public partial class LoginView : UserControl
 {
     private const string LoginUrl = @"https://www.armstrongpowerhouse.com/index.php?route=account/login";
+    private const string LogoutUrl = @"https://www.armstrongpowerhouse.com/index.php?route=account/logout";
     private static readonly HttpClientHandler _handler = new() {AllowAutoRedirect = false};
-    private readonly HttpClient _client = new(_handler);
 
     public LoginView()
     {
         InitializeComponent();
     }
 
+    public HttpClient Client { get; } = new(_handler);
+
     private async void Login(object sender, RoutedEventArgs e)
     {
         var content = new MultipartFormDataContent();
         content.Add(new StringContent(EmailBox.Text), "email");
         content.Add(new StringContent(PasswordBoxName.Password), "password");
-        var response = await _client.PostAsync(LoginUrl, content);
-        LoginResult.Text = response.StatusCode == HttpStatusCode.Redirect ? "Login Successful" : "Login Failed";
+        var response = await Client.PostAsync(LoginUrl, content);
+        if (response.StatusCode == HttpStatusCode.Redirect)
+        {
+            LoginResult.Text = "Login Successful";
+            LoginButton.IsEnabled = false;
+            LogoutButton.IsEnabled = true;
+        }
+        else
+        {
+            LoginResult.Text = "Login Failed";
+        }
+    }
+
+    private void Logout(object sender, RoutedEventArgs e)
+    {
+        Client.GetAsync(LogoutUrl);
+        LoginButton.IsEnabled = true;
+        LogoutButton.IsEnabled = false;
+        LoginResult.Text = "Logged out";
     }
 }
