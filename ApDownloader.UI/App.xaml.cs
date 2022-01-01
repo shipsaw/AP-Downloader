575a1f560ebdb4c5b42cd1f3603f2a53c52112a9
Paul Kirby <kirby.paulh@gmail.com>
2022-01-01 14:27:54 -0500

    Added basic exception logging for testing

--- ApDownloader.UI/App.xaml.cs
+++ ApDownloader.UI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using ApDownloader.UI.Logging;
 using ApDownloader.UI.MVVM.ViewModels;
 using ApDownloader.UI.MVVM.Views;
 
@@ -11,11 +13,22 @@ public partial class App : Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
+        var currentDomain = AppDomain.CurrentDomain;
+        currentDomain.UnhandledException +=
+            OnUnhandledException;
+
         MainWindow = new MainWindow
         {
             DataContext = new MainViewModel()
         };
         MainWindow.Show();
         base.OnStartup(e);
+    }
+
+    private static void OnUnhandledException(
+        object sender, UnhandledExceptionEventArgs e)
+    {
+        var exceptionStr = e.ExceptionObject.ToString();
+        Logger.LogFatal(exceptionStr);
     }
 }
---
+++ ApDownloader.UI/Logging/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ApDownloader.UI.Logging;
+
+public static class Logger
+{
+    public static void LogFatal(string? exceptionString)
+    {
+        if (exceptionString != null) File.WriteAllBytes(@".\log.txt", Encoding.ASCII.GetBytes(exceptionString));
+    }
+}
