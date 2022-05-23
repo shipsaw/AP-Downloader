0c5d259c7fd70da08344c50b061e63054941b74a
Paul Kirby <kirby.paulh@gmail.com>
2022-05-22 23:10:43 -0400

    Cleanup of unnecessary admin status checks

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"fmt"
 	"github.com/briandowns/spinner"
-	"golang.org/x/sys/windows"
 	"io"
 	"os"
 	"os/exec"
@@ -26,13 +25,8 @@ func main() {
 		fmt.Scanf("h")
 		os.Exit(1)
 	}
+	defer os.Remove(tempDir)
 	os.Remove(filepath.Join(tempDir, `install.log`))
-
-	//Check for correct permissions, un-comment this for release build
-	//if !checkElevatedStatus() {
-	//	fmt.Println("Error getting correct user permissions")
-	//	//fmt.Println("Error getting correct user permissions")
-	//}
 
 	manifest, err := os.Open(`C:\ProgramData\ApDownloader\Downloads.txt`)
 	if err != nil {
@@ -184,48 +178,6 @@ func getInstallLocation(scanner *bufio.Scanner) string {
 	}
 	return scanner.Text()
 
-}
-
-func checkElevatedStatus() bool {
-	var sid *windows.SID
-	// Although this looks scary, it is directly copied from the
-	// official windows documentation. The Go API for this is a
-	// direct wrap around the official C++ API.
-	// See https://docs.microsoft.com/en-us/windows/desktop/api/securitybaseapi/nf-securitybaseapi-checktokenmembership
-	err := windows.AllocateAndInitializeSid(
-		&windows.SECURITY_NT_AUTHORITY,
-		2,
-		windows.SECURITY_BUILTIN_DOMAIN_RID,
-		windows.DOMAIN_ALIAS_RID_ADMINS,
-		0, 0, 0, 0, 0, 0,
-		&sid)
-	if err != nil {
-		//fmt.Printlnf("SID Error: %s", err)
-		fmt.Println("SID Error: %s", err)
-		return false
-	}
-
-	// This appears to cast a null pointer so I'm not sure why this
-	// works, but this guy says it does and it Works for Me™:
-	// https://github.com/golang/go/issues/28804#issuecomment-438838144
-	token := windows.Token(0)
-
-	member, err := token.IsMember(sid)
-	if err != nil {
-		//fmt.Printlnf("Token Membership Error: %s", err)
-		fmt.Println("Token Membership Error: %s", err)
-		return false
-	}
-
-	// Also note that an admin is _not_ necessarily considered
-	// elevated.
-	// For elevation see https://github.com/mozey/run-as-admin
-	if token.IsElevated() && member {
-		fmt.Println(token.IsElevated(), member)
-		return true
-	}
-	fmt.Println(token.IsElevated(), member)
-	return false
 }
 
 func DecodeUTF16(b []byte) (string, error) {
