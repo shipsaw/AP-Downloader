84864dd0e2463c4f46d7997a09deffe7fa1108b9
Paul Kirby <kirby.paulh@gmail.com>
2023-03-19 11:29:49 -0400

    Test commit to check hook

---
+++ ApDownloader.DataAccess/dbToCsv.ps1
@@ -0,0 +1,10 @@
+param (
+    $dbPath = '.\ProductsDb.db',
+    $exportPath = '.\productsCSV.csv'
+)
+$db = New-SqliteConnection $dbPath
+Invoke-SqliteQuery -DataSource $dbPath -Query "select P.ProductID, P.Name, P.Filename, P.ImageName as 'Preview Image', BP.Filename as 'Branding Patch', ES.Filename as 'Extra Stock', LP.Filename as 'Livery Pack' from Product AS P
+left outer join BrandingPatch BP on P.ProductID = BP.ProductID
+left outer join ExtraStock ES on P.ProductID = ES.ProductID
+left outer join LiveryPack LP on P.ProductID = LP.ProductID" |
+export-csv $exportPath
